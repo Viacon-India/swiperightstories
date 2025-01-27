c750e17b733551d04c165e3c0cbbc26d739209ee
Md Adnan <md.adnan@viaconteam.com>
2025-01-27 11:48:56 +0530

    page layout done

---
+++ wp-content/themes/swipe-rights/author.php
@@ -0,0 +1,47 @@
+<?php /* Template Name: Author Page Template */ ?>
+
+<?php get_header(); ?>
+
+<div class="author-page pt-[68px]">
+    <div class="container mx-auto">
+        <div class="author-card">
+            <div class="w-fit flex gap-2">
+                <a href="">
+                    <figure class="mb-0 relative w-[60px] h-[60px] md:w-[100px] md:h-[100px] border border-[#101010]">
+                        <img class="author-img" src="<?php echo get_template_directory_uri(); ?>/images/card1.jpg" alt="author-img" />
+                    </figure>
+                </a>
+                <div class="md:hidden">
+                    <h2 class="author-title"><a href="">Akash Kumar</a></h2>
+                    <span class="author-designation">designation</span>
+                </div>
+            </div>
+            <div class="author-detail">
+                <h2 class="author-title hidden md:block"><a href="">Akash Kumar</a></h2>
+                <span class="author-designation hidden md:block">designation</span>
+                <p class="author-card-desc">The cybersports market in the U.S. and beyond is an ever-growing industry as more and more gamers around the world participate in online competitions and tournaments. According to the latest data compiled by Profiler, the global cybersports market is estimated to be worth around $1 billion in 2020 and is projected to reach a staggering $2.5 billion in 2025.
+                </p>
+            </div>
+        </div>
+        <div class="grid grid-cols-1 lg:grid-cols-3 gap-5 mt-10">
+            <?php
+            for ($i = 0; $i < 9; $i++) {
+                echo '
+    <div class="default-card">
+        <figure>
+            <img src="' . get_template_directory_uri() . '/images/card1.jpg" alt="card" />
+        </figure>
+        <div class="default-card-detail">
+            <p>outfits</p>
+            <h2>Sézane’s New Spring Collection is Full of Pieces You’ll Never Get Sick Of Wearing</h2>
+        </div>
+    </div>';
+            }
+            ?>
+        </div>
+        <div class="flex justify-center">
+            <button class="single-page-comment-from-submit-button">More Articles</button>
+        </div>
+    </div>
+</div>
+<?php get_footer(); ?>
--- wp-content/themes/swipe-rights/templates/about-us.php
+++ wp-content/themes/swipe-rights/templates/about-us.php
@@ -1,18 +1,71 @@
-<?php
-/* Template Name: Aout Us Page */
+<?php /* Template Name: About Page Template */ ?>
 
-$myvals = get_post_meta(get_the_ID());
+<?php get_header(); ?>
 
-// print_r($myvals);
+<section class="inner-sec pt-[68px]">
+  <div class="container mx-auto">
+    <div class="w-page-wrapper">
+      <div class="flex justify-center pt-[56px]">
+        <h2 class="about-title">About Us</h2>
+      </div>
+      <div class="page-content">
+        <p>Welcome to Emblem Wealth – A platform for business owners, entrepreneurs, writers, engaged readers and bloggers to read, write and support stories or ideas and share experiences. Emblem Wealth is the most trending business blog and digital content curated hub spot. It is committed to formulating a business strategy that suits your business career, needs, and requirements. We established it for the purpose of helping small business owners and entrepreneurs for their expanding globally and sharing their experiences through it.
+        </p>
+        <p>Has several skills to slow down and stun the target, and has good burst damage, and short cooldowns.
+        </p>
+        <p>You can read more information about other variations of the hunter class development in the WoW Classic Blog, a specialized resource with up-to-date information on Wow Wotlk and Dragonflight.
+        </p>
+        <p>Keep reading this article till the end to learn more about how to play the Unblocked Subway Surfers like a pro…
+        </p>
+        <h2>What Is Subway Surfers Unblocked?</h2>
+        <ol>
+          <li>This town ranked the top position out of 12 best things to do in Flagstaff. When you plan your Flagstaff trip, I would recommend that you start the trip from Historic Downtown and Railroad District.
+          </li>
+          <li>I’m sure you’re going to enjoy the beauty of the town walking along the streets. You will find the historic buildings which are now being converted into hotels, restaurants, breweries, etc.
+          </li>
+          <li>Even in the late night, you’ll find a crowd in the town. I remember when I came back from a movie show, some college students were out on the street. You can book a day or two of your stay here to enjoy the farmer’s market, local events, entertainment shows, shopping, eating, and drinking</li>
+          <li>The town allows easy access to many other tourist spots in Flagstaff. A wonderful park is located within walking distance from the visitor’s center.
+          </li>
+        </ol>
+        <p>Are you searching for Subway Surfers Unblocked? If your answer is a YES, then you will find this blog to be of help.
+        </p>
+        <p>There are several games that we play online or with the help of smartphones. These are the games that you do not need any specific gaming laptops for playing. These are free and enjoyable. And Subway Surfers is one of them
+        </p>
+        <p>Considering the fact that many schools and colleges have currently blocked several HTML5 and Flash gaming websites, people are focusing on unblocked platforms. If you have been searching about the ways in which you can play this game on your electronic gadgets without having to download or install them, you will find this blog to be of help.
+        </p>
+        <h3>How To Play Subway Surfers Unblocked?</h3>
+        <ul>
+          <li>To Move right: Arrow Key Right or D Key</li>
+          <li>To move left: Arrow Key Left or A key</li>
+          <li>To move forward: Arrow Key Upward or W key</li>
+          <li>To Jump: Spacebar</li>
+        </ul>
+        <h4>Challenges In The Game</h4>
+        <p>Studies show that 1 in 4 women and 1 in 9 children are victims of domestic abuse.
+          This is a growing problem for a progressive country like the U.S., and it’s time the government takes
+          an action to put a stop to this torture.
+        </p>
+        <h5>What Other Unblocked Games Are There?</h5>
 
-foreach ($myvals as $key => $val) {
-    
+        <p>Are you searching for Subway Surfers Unblocked? If your answer is a YES, then you will find this blog to be of help.
+        </p>
+        <h2>Frequently Asked Questions (FAQS):-</h2>
+        <p>Domestic violence is a common problem in the U.S. Every year, more than 10 million people are
+          subjected to domestic violence in the nation.
+        </p>
+        <p>Studies show that 1 in 4 women and 1 in 9 children are victims of domestic abuse.
+          This is a growing problem for a progressive country like the U.S., and it’s time the government takes
+          an action to put a stop to this torture.
+        </p>
+        <h3>How Do We Work?</h3>
+        <p>All that you need to do is scroll down till you reach the bottom of the page. Then leave your doubts and queries in the content box below. Do not forget to share what other unblocked games you would want to know about. Till then, stay safe!
+        </p>
+        <p>We are totally aware of the fact that the viewpoint of a writer in a story can differ, and due to this very context, we always prioritize our readers and their need from us. For any guest post requests, blogger queries, advertisements, and feedback, you can use the <a class="" href="">contact form</a> or mail us at <a href="">webmaster@redhatmedia.net.</a> After you have filled in with all the required details, we will get back to you as soon as possible.
+        </p>
 
-    ${$key} = $val[0] ?
-     unserialize($val[0]) :
-      $val[0];
+      </div>
+    </div>
+  </div>
+</section>
 
-    // echo $key .' - '. $val[0].'<br>';
-    echo $val[0];
-}
-
+<?php get_footer(); ?>
