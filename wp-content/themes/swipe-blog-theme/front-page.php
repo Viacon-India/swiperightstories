7e3413d846e9772286188d56242e7f2ef05671c7
subhamayviaconteam <subhamay.aditya@viaconteam.com>
2026-06-11 11:50:17 +0530

    thumbnail size resizes

--- wp-content/themes/swipe-blog-theme/front-page.php
+++ wp-content/themes/swipe-blog-theme/front-page.php
@@ -360,7 +360,12 @@ $category_four_query = new WP_Query(
                     <div class="hero-img-sec">
                         <figure class="w-full h-[254px] md:h-[517px]">
                             <?php if (has_post_thumbnail($recent_posts[2]->ID)) : ?>
-                                <?php echo get_the_post_thumbnail($recent_posts[2]->ID, 'related-thumbnail', array('class' => 'w-full h-full object-contain')); ?>
+                                <?php /* echo get_the_post_thumbnail($recent_posts[2]->ID, 'related-thumbnail', array('class' => 'w-full h-full object-contain')); */ ?>
+                                <?php echo get_the_post_thumbnail(
+                                    $recent_posts[2]->ID,
+                                    'large',
+                                    array('class' => 'w-full h-auto')
+                                ); ?>
                             <?php else : ?>
                                 <img src="<?php echo get_template_directory_uri(); ?>/images/card3.jpg" class="w-full h-full object-contain" alt="card" />     
                             <?php endif; ?>
