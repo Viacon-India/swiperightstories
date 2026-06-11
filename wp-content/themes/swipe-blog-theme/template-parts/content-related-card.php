8015aa5fbf07aeee766ffac41ce0d0bd97af2cbc
subhamayviaconteam <subhamay.aditya@viaconteam.com>
2026-06-11 11:58:38 +0530

    thumbnail image resizes

--- wp-content/themes/swipe-blog-theme/template-parts/content-related-card.php
+++ wp-content/themes/swipe-blog-theme/template-parts/content-related-card.php
@@ -9,11 +9,17 @@ $cat_Name = $cat[0]->cat_name;
 <div class="default-card">
     <a href="<?php echo get_the_permalink($post_ID); ?>">
         <?php if (has_post_thumbnail()) : ?>
-            <?php echo get_the_post_thumbnail($post_ID, 'related-thumbnail'); ?>
+            <?php echo get_the_post_thumbnail(
+                $post_ID,
+                'large',
+                array(
+                    'class' => 'w-full h-auto block'
+                )
+            ); ?>
         <?php else : ?>
-            <figure>
-                <img src="<?php echo get_template_directory_uri(); ?>/images/card1.jpg" alt="card" />
-            </figure>
+            <img src="<?php echo get_template_directory_uri(); ?>/images/card1.jpg"
+                 class="w-full h-auto block"
+                 alt="card">
         <?php endif; ?>
     </a>
     <div class="default-card-detail">
